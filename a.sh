f6c24709fd34410eb79f4c295fffa79dcf94865a
nobody <nobody@7dd34454-f5d5-49e4-b66a-dc347b6f32b8>
2017-12-09 10:07:43 +0000

    ee

--- a.sh
+++ a.sh
@@ -4,6 +4,6 @@ mkdir /hello
 touch /hello/hello.txt
 mkdir -p /test/test1
 touch /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
+echo "hello1 hello" > /hello/hello.txt
 echo "world world" > /test/test1/world.txt
 cp -r /hello/* $ALAUDACD_UPLOAD_DIR
