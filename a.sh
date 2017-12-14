8aaa541db63bbb49620afbd4a10ec6a4f1de5d89
nobody <nobody@7dd34454-f5d5-49e4-b66a-dc347b6f32b8>
2017-12-14 03:42:46 +0000

    sds

--- a.sh
+++ a.sh
@@ -4,6 +4,6 @@ mkdir /hello
 touch /hello/hello.txt
 mkdir -p /test/test1
 touch /test/test1/world.txt
-echo "hellosdfsf1 hello" > /hello/hello.txt
+echo "hellosf1 hello" > /hello/hello.txt
 echo "world world" > /test/test1/world.txt
 cp -r /hello/* $ALAUDACD_UPLOAD_DIR
